3911c564897f550fcb9607de32be1f4151ea0657
Ray <rjmaestas1991@gmail.com>
2022-11-16 09:19:53 -0700

    css

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-//test
+fetch('http://localhost:3000')
+    .then(r => r.json())
+    .then(json => {
+        drinkData = json
+        drinkData.forEach(
+            drink => {
+                createImage(drink)
+            }
+        )
+        drinkDetails(drinkData[0])
+
+    })
+function createImage(ramen) {
+    let ramenMenu = document.querySelector('#ramen-menu')
+    let ramenImage = document.createElement('img')
+    ramenImage.src = ramen.image
+    ramenMenu.appendChild(ramenImage)
+
+}
+
