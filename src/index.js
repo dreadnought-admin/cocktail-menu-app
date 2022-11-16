e2542fe02ab07f32ec4af6cdf360600d1b225fe4
Ray <rjmaestas1991@gmail.com>
2022-11-16 16:19:37 -0700

    added 2 pictures

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 fetch("http://localhost:3000/beers")
     .then(r => r.json())
     .then(json => {
-        beerData = json
+        beerData = json.filter(beer => beer.id<3)
         beerData.forEach(
             beer => {
                 beerImage(beer)
@@ -10,13 +10,30 @@ fetch("http://localhost:3000/beers")
 
     })
 
-function beerImage(beer) {
+function beerImage(beer){
     let beerList = document.querySelector("#static-beer-list")
     let favList = document.createElement("li")
     let beerImage = document.createElement("img")
     beerImage.src = beer.image_url;
-    beerList.appendChild(beerImage);
     beerList.appendChild(favList);
-
+    favList.appendChild(beerImage);
 
 }
+
+// const url = ("https://api.punkapi.com/v2/beers/")
+// let currentBeer;
+// fetch(url)
+//     .then(r => r.json())
+//     .then(json => {
+//         renderImages(json[0]);
+//     });
+// function renderImages(beer) {
+//     Object.keys(beer).forEach(function (key) {
+//         let staticFavs = document.querySelector("#static-beer-list");
+//         let staticList = document.createElement("li");
+//         staticFavs.appendChild(staticList);
+//         let staticImages = document.createElement("img");
+//         staticImages.src = beer.image_url;
+//         staticList.appendChild(staticImages);
+//     });
+// }
